a056b90eeb8796ce56488089ddb43433f2ddb13f
unknown <bajiraouttamsinha@gmail.com>
2022-04-19 18:51:33 +0530

    replacing img with Image

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from 'next/image'
 
 import Navbar from "../navbar";
 import classes from "./index.module.css";
@@ -14,21 +15,21 @@ const Dashboard = () => {
             </div>
             <div className={classes.options}>
                 <div className={classes.windows}>
-                    <img src="/assets/win.png" alt="win"/>
+                    <Image width="250px" height="100%" src="/assets/win.png" alt="win"/>
                     <div className={classes.content}>
                         <p>Try our exclusive <span className={classes.span}>Windows Simulator</span></p>
                         <Link href="./windows"><button name="win" className={classes.btn} type="submit">Select</button></Link>
                     </div>
                 </div>
                 <div className={classes.linux}>
-                    <img src="/assets/linux.png" alt="linux"/>
+                    <Image width="205px" height="100%" src="/assets/linux.png" alt="linux"/>
                     <div className={classes.content}>
                         <p>Try our exclusive <span className={classes.span}>Linux simulator</span></p>
                         <Link href="./linux"><button name="linux" className={classes.btn}  type="submit">Select</button></Link>
                     </div>
                 </div>
                 <div className={classes.mac}>
-                    <img src="/assets/mac.png" alt="mac"/>
+                    <Image width="300px" height="100%" src="/assets/mac.png" alt="mac"/>
                     <div className={classes.content}>
                         <p>Try our exclusive <span className={classes.span}> Mac Simulator </span></p>
                         <Link href="./mac"><button name="mac" className={classes.btn}  type="submit">Select</button></Link>
