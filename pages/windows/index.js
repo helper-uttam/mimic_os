4ff012b59f68bdb9d3a0b6d41141debcce4d43d3
unknown <bajiraouttamsinha@gmail.com>
2022-04-22 22:39:13 +0530

    fixing icons and buttons UI

--- pages/windows/index.js
+++ pages/windows/index.js
@@ -12,26 +12,26 @@ const Windows = () => {
             <div className={classes.icons}>
             <Link href="/windows/bin">
                 <div className={classes.icon}>
-                    <Image width={120} height={100} src="/assets/windows/bin.png" alt="recyclebin" />
+                    <Image width={100} height={90} src="/assets/windows/bin.png" alt="recyclebin" />
                     <p className={classes.p}>Recycle Bin</p>
                 </div>
             </Link>
             <Link href="/windows/chrome">
-                <div className={classes.iconC}>
-                    <Image width={150} height={100} src="/assets/windows/chrome.png" alt="chrome" />
+                <div className={classes.icon}>
+                    <Image width={160} height={90} src="/assets/windows/chrome.png" alt="chrome" />
                     <p className={classes.p}>Chrome</p>
                 </div>
             </Link>
             <Link href="/windows/edge">
                 <div className={classes.icon}>
-                    <Image width={100} height={100} src="/assets/windows/edge.png" alt="edge" />
+                    <Image width={85} height={90} src="/assets/windows/edge.png" alt="edge" />
                     <p className={classes.p}>Edge</p>
                 </div>
             </Link>
             <Link href="/windows/cli">
                 <div className={classes.icon}>
                     <Image width={100} height={100} src="/assets/windows/cmd.webp" alt="cmd" />
-                    <p className={classes.p}>Command Prompt</p>
+                    <p style={{marginTop:"5%"}} className={classes.p}>Command <br /> Prompt</p>
                 </div>
             </Link>
         </div>
